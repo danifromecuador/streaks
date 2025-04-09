97ba4896f65a5a4e1164ab025923c4706b7ec1c0
dani <danifromecuador@gmail.com>
2025-04-09 00:37:53 -0500

    feat: show edit & delete btns just when hovering

--- src/components/streaks/Streak.jsx
+++ src/components/streaks/Streak.jsx
@@ -1,13 +1,15 @@
+import { useState } from 'react'
 import './Streak.css'
 
 export const Streak = () => {
+  const [visible, setVisible] = useState('hide')
   return (
-    <div className='streak'>
-      <a className='streak-container' href="https://i.imgur.com/dqz5lqQ.jpeg"  target="_blank" >
+    <div className='streak' onMouseEnter={() => setVisible('')} onMouseLeave={() => setVisible('hide')}>
+      <a className='streak-container' href="https://i.imgur.com/dqz5lqQ.jpeg" target="_blank" >
         <img className='streak-bg' src="https://i.imgur.com/dqz5lqQ.jpeg" alt="" />
       </a>
-      <button className='btn streak-edit-btn'>E</button>
-      <button className='btn streak-delete-btn'>D</button>
+      <button className={`btn streak-edit-btn ${visible}`}>E</button>
+      <button className={`btn streak-delete-btn ${visible}`}>D</button>
     </div>
   )
 }
