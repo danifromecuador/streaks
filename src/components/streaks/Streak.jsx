d0d48f2104e2d8238767797007dea48b5a0206f7
dani <danifromecuador@gmail.com>
2025-04-09 00:24:25 -0500

    style: mantain relative size of elements inside Streak.jsx

--- src/components/streaks/Streak.jsx
+++ src/components/streaks/Streak.jsx
@@ -3,7 +3,11 @@ import './Streak.css'
 export const Streak = () => {
   return (
     <div className='streak'>
-      Streak
+      <a className='streak-container' href="https://i.imgur.com/dqz5lqQ.jpeg"  target="_blank" >
+        <img className='streak-bg' src="https://i.imgur.com/dqz5lqQ.jpeg" alt="" />
+      </a>
+      <button className='btn streak-edit-btn'>E</button>
+      <button className='btn streak-delete-btn'>D</button>
     </div>
   )
 }
