f0612d5572709b38c146f0974add70479a08f430
dani <danifromecuador@gmail.com>
2025-04-09 21:03:37 -0500

    feat: show add btn just when hovering Streaks.jsx

--- src/components/streaks/Streaks.jsx
+++ src/components/streaks/Streaks.jsx
@@ -1,23 +1,18 @@
+import { useState } from 'react'
 import { Streak } from './Streak'
 import './Streaks.css'
 
 export const Streaks = () => {
+  const [visible, setVisible] = useState('hidden')
+
   return (
-    <div className='streaks'>
+    <div className='streaks' onMouseEnter={() => setVisible('')} onMouseLeave={() => setVisible('hidden')} >
       <div className="streaks-list">
         <Streak />
         <Streak />
         <Streak />
-        <Streak />
-        <Streak />
-        <Streak />
-        <Streak />
-        <Streak />
-        <Streak />
-        <Streak />
-        <Streak />
       </div>
-      <button className='btn add-streak-btn'>+</button>
+      <button className={`btn add-streak-btn ${visible}`}>+</button>
     </div>
   )
 }
