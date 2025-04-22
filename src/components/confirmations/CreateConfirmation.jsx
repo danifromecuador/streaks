a576c0fb68ab65d1c3091fde943dbfb859dbe080
dani <danifromecuador@gmail.com>
2025-04-22 11:59:23 -0500

    chore: add or create a new streak to streaks array

--- src/components/confirmations/CreateConfirmation.jsx
+++ src/components/confirmations/CreateConfirmation.jsx
@@ -1,24 +1,33 @@
 import { useState } from 'react'
+import { Store } from '../../store/Store'
 import './Confirmations.css'
 
 export const CreateConfirmation = () => {
+  const store = Store()
   const [name, setName] = useState("")
   const [url, setUrl] = useState("")
-  console.log(name);
-  
+
+  const addStreak = () => {
+    const data = {
+      name: name,
+      url: url
+    }
+    store.addStreak(data)
+  }
+
 
   return (
     <div className='confirmations'>
       <h2>Create new Streak</h2>
       <div>
         <p>name</p>
-        <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
       </div>
       <div>
         <p>url</p>
-        <input type="text" value={url} onChange={((e)=>setUrl(e.target.value))}/>
+        <input type="text" value={url} onChange={((e) => setUrl(e.target.value))} />
       </div>
-      <button type="submit">Create</button>
-    </div>
+      <button type="submit" onClick={() => addStreak()}>Create</button>
+    </div >
   )
 }
--- src/store/Store.js
+++ src/store/Store.js
@@ -3,5 +3,7 @@ import { devtools } from 'zustand/middleware'
 
 export const Store = create(devtools((set) => ({
   visible1: true,
-  toggleVisible1: () => set((state) => ({ visible1: !state.visible1 }))
+  toggleVisible1: () => set((state) => ({ visible1: !state.visible1 })),
+  streaks: [],
+  addStreak: (data)=> set((state)=>({streaks: [...state.streaks, data]}))
 })))
