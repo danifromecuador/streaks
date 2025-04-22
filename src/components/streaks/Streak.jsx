03ffa294ef18e3f8ee13c38ddf8817c4922ff700
dani <danifromecuador@gmail.com>
2025-04-22 17:45:42 -0500

    chore: render all the streaks stored in the store

--- src/components/streaks/Streak.jsx
+++ src/components/streaks/Streak.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import './Streak.css'
 
-export const Streak = () => {
+export const Streak = ({name, image, url}) => {
   const [visible, setVisible] = useState('hide')
   return (
     <div className='streak' onMouseEnter={() => setVisible('')} onMouseLeave={() => setVisible('hide')}>
-      <a className='streak-container' href="https://i.imgur.com/dqz5lqQ.jpeg" target="_blank" rel='noopener noreferrer'>
-        <img className='streak-bg' src="https://i.imgur.com/dqz5lqQ.jpeg" alt="" />
+      <a className='streak-container' href={`https://${url}`} target="_blank" rel='noopener noreferrer'>
+        <img className='streak-bg' src={image} alt={name} />
       </a>
       <button className={`btn streak-edit-btn ${visible}`}>E</button>
       <button className={`btn streak-delete-btn ${visible}`}>D</button>
-      <span>Label</span>
+      <span>{name}</span>
     </div>
   )
 }
--- src/components/streaks/Streaks.jsx
+++ src/components/streaks/Streaks.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
+import { Store } from '../../store/Store'
 import { Streak } from './Streak'
 import './Streaks.css'
 
 export const Streaks = () => {
+  const store = Store()
+  const streaks = store.streaks
   const [visible, setVisible] = useState('hide')
 
   return (
     <div className='streaks' onMouseEnter={() => setVisible('')} onMouseLeave={() => setVisible('hide')} >
-      <Streak />
-      <Streak />
-      <Streak />
-      <Streak />
+      {streaks.map((e) => (
+        <Streak name={e.name} image={e.image} url={e.url} />
+      ))}
       <button className={`btn add-streak-btn streak ${visible}`}>+</button>
     </div>
   )
