a138b86156c6b1bc95e69fd9e87609bdcac9ab4b
dani <danifromecuador@gmail.com>
2025-04-27 19:10:05 -0500

    feat: delete a streak

--- src/components/confirmations/DeleteConfirmation.jsx
+++ src/components/confirmations/DeleteConfirmation.jsx
@@ -4,14 +4,18 @@ import './Confirmations.css'
 export const DeleteConfirmation = () => {
   const store = Store()
   const visibility = store.visible3 ? "" : "hide"
+  const deleteStreak = () => {
+    store.deleteStreak()
+    store.toggleVisible3()
+  }
 
   return (
     <div className={`confirmations ${visibility}`}>
       <h2>Are you sure to delete this Streak?</h2>
       <button className='btn close-btn' onClick={() => store.toggleVisible3()}>X</button>
       <span>You will lost your streak!</span>
-      <button>DELETE</button>
-      <button>CANCEL</button>
+      <button className='btn' onClick={deleteStreak}>DELETE</button>
+      <button className='btn' onClick={() => store.toggleVisible3()}>CANCEL</button>
     </div>
   )
 }
--- src/components/streaks/Streak.jsx
+++ src/components/streaks/Streak.jsx
@@ -5,13 +5,17 @@ import './Streak.css'
 export const Streak = ({ name, image, url }) => {
   const store = Store()
   const [visible, setVisible] = useState('hide')
+  const deleteStreak = () => {
+    store.toggleVisible3()
+    store.setStreakNameToDelete(name)
+  }
   return (
     <div className='streak' onMouseEnter={() => setVisible('')} onMouseLeave={() => setVisible('hide')}>
       <a className='streak-container' href={`https://${url}`} target="_blank" rel='noopener noreferrer'>
         <img className='streak-bg' src={image} alt={name} />
       </a>
       <button className={`btn streak-edit-btn ${visible}`}>E</button>
-      <button className={`btn streak-delete-btn ${visible}`} onClick={() => store.toggleVisible3()}>D</button>
+      <button className={`btn streak-delete-btn ${visible}`} onClick={deleteStreak}>D</button>
       <span>{name}</span>
     </div>
   )
