2369cc8d9c0d461deea1c9f2165aa68fce5348a8
Daniel Morillo <danifromecuador@gmail.com>
2025-06-17 01:17:41 -0500

    chore: conditional rendering of edit and delete dialogs

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -1,15 +1,17 @@
+import { Store } from '../../store/Store'
 import { CreateConfirmation } from '../confirmations/CreateConfirmation'
 import { EditConfirmation } from '../confirmations/EditConfirmation'
 import { DeleteConfirmation } from '../confirmations/DeleteConfirmation'
 import './Grid.css'
 
 export const Grid = () => {
+  const store = Store()
   return (
     <div className='grid'>
       <div className="grid-container"> 1 2 3</div>
       <CreateConfirmation />
-      <EditConfirmation />
-      <DeleteConfirmation />
+      {store.streakIdToEdit && <EditConfirmation />}
+      {store.streakIdToDelete && <DeleteConfirmation />}
     </div>
   )
 }
