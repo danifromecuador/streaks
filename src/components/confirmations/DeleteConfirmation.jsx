99d6b8a3619c62b95e9f8691a6de6cd55d79bd26
Daniel Morillo <danifromecuador@gmail.com>
2025-07-18 11:43:45 -0500

    refactor: reuse Streak components for Shortcut stuff

--- src/components/confirmations/DeleteConfirmation.jsx
+++ src/components/confirmations/DeleteConfirmation.jsx
@@ -4,15 +4,29 @@ import './Confirmations.css'
 
 export const DeleteConfirmation = () => {
   const store = Store()
-  const streakNameToDelete = store.streaks.find(streak => streak.id === store.streakIdToDelete).name
+  const streakOrShortcutToDelete = store.streaks.find(streak => streak.id === store.streakIdToDelete) ||
+    store.shortcuts.find(shortcut => shortcut.id === store.shortcutIdToDelete)
+  let streakOrShortcut = ''
+  if (store.streakIdToDelete) streakOrShortcut = 'streak'
+  if (store.shortcutIdToDelete) streakOrShortcut = 'shortcut'
+
+  const closeBtn = () => {
+    store.setStreakIdToDelete(null)
+    store.setShortcutIdToDelete(null)
+  }
+
+  const deleteBtn = () => {
+    if (store.streakIdToDelete) store.deleteStreak()
+    if (store.shortcutIdToDelete) store.deleteShortcut()
+  }
 
   return (
     <div className={`confirmations`}>
-      <h2>Are you sure to delete {streakNameToDelete}?</h2>
-      <button className='btn close-btn' onClick={() => store.setStreakIdToDelete(null)}><CloseIcon /></button>
-      <p className='warning'>You will lost your streak!</p>
+      <h2>Are you sure to delete {streakOrShortcut} {streakOrShortcutToDelete.name}?</h2>
+      <button className='btn close-btn' onClick={closeBtn}><CloseIcon /></button>
+      {store.streakIdToDelete && <p className='warning'>You will lost your streak!</p>}
       <div className='delete-cancel-btns'>
-        <button className='btn delete' onClick={() => store.deleteStreak()}>DELETE</button>
+        <button className='btn delete' onClick={deleteBtn}>DELETE</button>
       </div >
     </div >
   )
--- src/components/streaks/Streak.jsx
+++ src/components/streaks/Streak.jsx
@@ -8,7 +8,7 @@ export const Streak = ({ id, name, image, url, type }) => {
   const store = Store()
   const [visible, setVisible] = useState('hide')
 
-  const editStreak = () => {
+  const edit = () => {
     // here we aren't editing, just setting the id to the streak or shortcut that is going to be edited
     // real edition takes place on edit confirmation dialog
     // show edit dialog and hide other dialogs
@@ -16,19 +16,22 @@ export const Streak = ({ id, name, image, url, type }) => {
     store.setStreakIdToEdit(null)
     store.setStreakIdToDelete(null)
     store.setShortcutIdToEdit(null)
-    // TODO store.setShortcutIdToDelete(null)
+    store.setShortcutIdToDelete(null)
     if (type === 'streaks') store.setStreakIdToEdit(id)
     if (type === 'shortcuts') store.setShortcutIdToEdit(id)
   }
 
-  const deleteStreak = () => {
+  const deleteFn = () => {
     // here we aren't deleting the streak, just setting the id to the streak that is going to be deleted
     // real deletion of that streak takes place on delete confirmation dialog
-    store.setStreakIdToDelete(id)
     // show delete streak dialog and hide other dialogs
     store.visible2 && store.toggleVisible2()
     store.setStreakIdToEdit(null)
-    store.setStreakIdToDelete(id)
+    store.setStreakIdToDelete(null)
+    store.setShortcutIdToEdit(null)
+    store.setShortcutIdToDelete(null)
+    if (type === 'streaks') store.setStreakIdToDelete(id)
+    if (type === 'shortcuts') store.setShortcutIdToDelete(id)
   }
 
   return (
@@ -36,8 +39,8 @@ export const Streak = ({ id, name, image, url, type }) => {
       <a className='streak-container' href={url} target="_blank" rel='noopener noreferrer'>
         <img className='streak-bg' src={image} alt={name} />
       </a>
-      <button className={`btn streak-edit-btn ${visible}`} onClick={editStreak}><EditIcon sx={{ fontSize: '1.5vw' }} /></button>
-      <button className={`btn streak-delete-btn ${visible}`} onClick={deleteStreak}><DeleteIcon sx={{ fontSize: '1.5vw' }} /></button>
+      <button className={`btn streak-edit-btn ${visible}`} onClick={edit}><EditIcon sx={{ fontSize: '1.5vw' }} /></button>
+      <button className={`btn streak-delete-btn ${visible}`} onClick={deleteFn}><DeleteIcon sx={{ fontSize: '1.5vw' }} /></button>
       <span>{name}</span>
     </div>
   )
