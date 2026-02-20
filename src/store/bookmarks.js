f09cf97dc4f41169b0af1dfd4aa08ebaf97788eb
Daniel Morillo <danifromecuador@gmail.com>
2026-02-20 14:55:03 -0500

    refactor: change indentation on store files

--- src/store/bookmarks.js
+++ src/store/bookmarks.js
@@ -2,12 +2,15 @@ import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 
 /** Global state for bookmarks: list plus add/delete. Persisted to localStorage under 'bookmarks'. */
-export const useBookmarkStore = create(devtools(persist(set => ({
-    bookmarks: [],
-    addBookmark: data => set(state => ({ bookmarks: [...state.bookmarks, data] })),
-    deleteBookmark: name => set(state => ({ bookmarks: state.bookmarks.filter(b => b.name !== name) })),
-  }),
-    { name: 'bookmarks', partialize: state => ({ bookmarks: state.bookmarks }) }
-  )
+export const useBookmarkStore = create(
+  devtools(
+    persist(
+      set => ({
+        bookmarks: [],
+        addBookmark: data => set(state => ({ bookmarks: [...state.bookmarks, data] })),
+        deleteBookmark: name => set(state => ({ bookmarks: state.bookmarks.filter(b => b.name !== name) })),
+      }),
+      { name: 'bookmarks', partialize: state => ({ bookmarks: state.bookmarks }) }
+    )
   )
 )
--- src/store/streaks.js
+++ src/store/streaks.js
@@ -2,12 +2,15 @@ import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 
 /** Global state for streaks: list plus add/delete. Persisted to localStorage under 'streaks'. */
-export const useStreakStore = create(devtools(persist(set => ({
-  streaks: [],
-  addStreak: data => set(state => ({ streaks: [...state.streaks, data] })),
-  deleteStreak: name => set(state => ({ streaks: state.streaks.filter(s => s.name !== name) })),
-}),
-  { name: 'streaks', partialize: state => ({ streaks: state.streaks }) }
-)
-)
+export const useStreakStore = create(
+  devtools(
+    persist(
+      set => ({
+        streaks: [],
+        addStreak: data => set(state => ({ streaks: [...state.streaks, data] })),
+        deleteStreak: name => set(state => ({ streaks: state.streaks.filter(s => s.name !== name) })),
+      }),
+      { name: 'streaks', partialize: state => ({ streaks: state.streaks }) }
+    )
+  )
 )
